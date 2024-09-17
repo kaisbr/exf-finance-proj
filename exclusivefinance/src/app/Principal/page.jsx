c896d49cb184ec27d39157ce4bbe2c57d7f840ce
Kai <62500174+kaisbr@users.noreply.github.com>
2024-09-17 03:16:26 +0000

    tela principal esboço

--- exclusivefinance/src/app/Principal/page.jsx
+++ exclusivefinance/src/app/Principal/page.jsx
@@ -1,35 +1,45 @@
+"use client";
 import Head from 'next/head'
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
-  return (
+
+export default function HomePage() {
+    const router = useRouter(); // Inicializa o hook
+  
+    const handleBackClick = () => {
+      router.push('/'); // Redireciona sempre para a página principal (app/page.jsx)
+    }; 
+    
+    return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <Head>
-        <title>Página Inicial</title>
-        <meta name="description" content="Página inicial da sua empresa" />
+        <title>Inicio</title>
+        <meta name="description" content="Página inicial do app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="w-full flex-1 flex flex-col items-center justify-center text-center">
         <h1 className="text-5xl font-bold text-gray-800">
-          Bem-vindo à Minha Empresa de Festas
+          Bem-vindo ao Exclusive Finance
         </h1>
 
         <p className="mt-3 text-xl text-gray-600">
-          Aluguel de festas incríveis para todos os momentos!
+          Cuidando de você e do seu bolso!
         </p>
 
         <div className="mt-5">
-          <a
-            href="#"
-            className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg hover:bg-blue-700 transition"
-          >
-            Saiba Mais
-          </a>
-        </div>
+        <button
+          onClick={handleBackClick}
+          className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
+        >
+          Voltar para Página Inicial
+        </button>
+      </div>
+
       </main>
 
       <footer className="w-full h-24 flex items-center justify-center border-t">
-        <p className="text-gray-500">© 2024 Minha Empresa de Festas</p>
+        <p className="text-gray-500">© 2024 EXF Exclusive Finance</p>
       </footer>
     </div>
   )
