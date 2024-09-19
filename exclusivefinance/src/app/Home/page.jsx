59761ee823b5743913379917c3031420f700fb9d
cleyton123 <143197269+cleyton123@users.noreply.github.com>
2024-09-19 14:19:04 +0000

    tela principal

--- exclusivefinance/src/app/Home/page.jsx
+++ exclusivefinance/src/app/Home/page.jsx
@@ -10,41 +10,35 @@ export default function HomePage() {
       router.push('/'); // Redireciona para a página de login (ou outra página padrão)
     };
 
-    // Função para redirecionar para a tela principal
-    const handleMainPageClick = () => {
-      router.push('/Home'); // Ajuste a rota para a página principal correta
-    };
-
     return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
       <Head>
-        <title>Inicio</title>
+        <p>Inicio</p>
         <meta name="description" content="Página inicial do app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="w-full flex-1 flex flex-col items-center justify-center text-center">
-        <h1 className="text-5xl font-bold text-gray-800">
-          Bem-vindo ao Exclusive Finance
-        </h1>
+        {/* Alinhando os elementos lado a lado */}
+        <div className="flex flex-row items-center -mt-40 space-x-40">
+          <h1 className="text-5xl font-bold text-gray-800">
+            Início
+          </h1>
+          <button className="text-xl text-gray-600 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition">
+            Categorias
+          </button>
+          <button className="text-xl text-gray-600 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition">
+            Lista de gastos
+          </button>
+        </div>
 
-        <p className="mt-3 text-xl text-gray-600">
-          Cuidando de você e do seu bolso!
-        </p>
-
+        {/* Botão abaixo dos textos */}
         <div className="mt-5 flex flex-col items-center space-y-5">
           <button
             onClick={handleBackClick} // Botão para voltar para a página inicial (login ou home)
-            className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
+            className="mt-40 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
           >
-            Voltar para Página Inicial
-          </button>
-
-          <button
-            onClick={handleMainPageClick} // Botão para redirecionar para a tela principal
-            className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
-          >
-            Tela Principal
+           adicionar gasto
           </button>
         </div>
       </main>
