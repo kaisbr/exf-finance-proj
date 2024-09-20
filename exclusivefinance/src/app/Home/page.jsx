94bfb22cc5d9ac7e6abc09f260a3024317aeb08f
Kai <62500174+kaisbr@users.noreply.github.com>
2024-09-20 12:16:54 +0000

    tentando implementar responsividade

--- exclusivefinance/src/app/Home/page.jsx
+++ exclusivefinance/src/app/Home/page.jsx
@@ -1,65 +1,69 @@
 "use client";
 import Head from 'next/head';
-import { useRouter } from 'next/navigation'; // Certifique-se de usar apenas este hook
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
     const router = useRouter(); // Inicializa o hook para navegação
 
-    // Função para redirecionar para a página inicial
-    const handleBackClick = () => {
-      router.push('/'); // Redireciona para a página de login (ou outra página padrão)
-    };
-
     return (
-      <div className="min-h-screen flex justify-center items-center bg-gray-100">
-        
-        {/* Onda azul no fundo */}
-      <svg className="absolute bottom-0 left-0 w-full h-full z-0" viewBox="0 0 1440 320" preserveAspectRatio="none">
-        <defs>
-          <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" style={{ stopColor: '#2168A9', stopOpacity: 1 }} />
-            <stop offset="52%" style={{ stopColor: '#6592E9', stopOpacity: 1 }} />
-            <stop offset="100%" style={{ stopColor: '#1E1E1C', stopOpacity: 1 }} />
-          </linearGradient>
-        </defs>
-        <path fill="url(#waveGradient)" fillOpacity="1" d="M0,160 C240,320 720,0 960,160 C1200,320 1680,0 1920,160 L1920,320 L0,320 Z"></path>
-      </svg>
+        <div className="min-h-screen flex justify-center items-center bg-gray-100 relative">
+            {/* Onda azul no fundo */}
+            <svg className="absolute bottom-0 left-0 w-full h-full z-0" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <defs>
+                    <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%" style={{ stopColor: '#2168A9', stopOpacity: 1 }} />
+                        <stop offset="52%" style={{ stopColor: '#6592E9', stopOpacity: 1 }} />
+                        <stop offset="100%" style={{ stopColor: '#1E1E1C', stopOpacity: 1 }} />
+                    </linearGradient>
+                </defs>
+                <path fill="url(#waveGradient)" fillOpacity="1" d="M0,160 C240,320 720,0 960,160 C1200,320 1680,0 1920,160 L1920,320 L0,320 Z"></path>
+            </svg>
 
-      <div className="bg-white bg-opacity-20 backdrop-blur-md text-white flex flex-col justify-center items-center p-10 rounded-lg w-[1100px] h-[500px] shadow-lg">
-          <Head>
-       
-      
-        <p>Inicio</p>
-        <meta name="description" content="Página inicial do app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+            <div className="bg-white bg-opacity-20 backdrop-blur-md text-white flex flex-col justify-start items-start p-10 rounded-lg min-w-[300px] max-w-[1100px] min-h-[500px] h-auto shadow-lg">
+                <Head>
+                    <title>Início</title>
+                    <meta name="description" content="Página inicial do app" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
 
-      <main className="w-full flex-1 flex flex-col items-center justify-center text-center">
-        {/* Alinhando os elementos lado a lado */}
-        <div className="flex flex-row items-center -mt-10 space-x-40">
-          <h1 className="text-5xl font-bold text-gray-800">
-            Início
-          </h1>
-          <button className="text-xl text-gray-600 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition">
-            Categorias
-          </button>
-          <button className="text-xl text-gray-600 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition">
-            Lista de gastos
-          </button>
+                <button 
+                    type="button" 
+                    onClick={() => router.push('/Principal')}
+                    className="absolute top-4 left-4 flex items-center p-2"
+                >
+                    <img 
+                        src="https://cdn-icons-png.flaticon.com/512/134/134226.png" 
+                        alt="seta de back" 
+                        className="w-5 h-5 mr-2"
+                    />
+                </button>
+
+                <main className="w-full flex flex-col items-start justify-start text-left mt-10">
+                    <div className="flex flex-col md:flex-row items-start md:space-x-4 mb-4">
+                        <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-4 md:mb-0">
+                            Início
+                        </h1>
+                        <div className="flex flex-col md:flex-row">
+                            <button className="text-black bg-white bg-opacity-40 border backdrop-blur-md text-lg md:text-xl px-4 py-2 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition mb-2 md:mb-0 md:mr-2">
+                                Categorias
+                            </button>
+                            <button className=" text-black bg-white bg-opacity-40 border backdrop-blur-md text-lg md:text-xl px-4 py-2 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition">
+                                Lista de gastos
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Botão abaixo dos textos */}
+                    <div className="flex flex-col items-center space-y-5 ">
+                        <button
+                            onClick={() => router.push('/novogasto')}
+                            className="text-black bg-white bg-opacity-40 border backdrop-blur-md px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
+                        >
+                            Adicionar gasto
+                        </button>
+                    </div>
+                </main>
+            </div>
         </div>
-
-        {/* Botão abaixo dos textos */}
-        <div className="mt-5 flex flex-col items-center space-y-5">
-          <button
-            onClick={handleBackClick} // Botão para voltar para a página inicial (login ou home)
-            className="mt-40 px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
-          >
-           adicionar gasto
-          </button>
-        </div>
-      </main>
-
-    </div>
-  </div>
-  );
+    );
 }
--- exclusivefinance/src/app/Principal/page.jsx
+++ exclusivefinance/src/app/Principal/page.jsx
@@ -1,68 +1,69 @@
 "use client";
 import Head from 'next/head';
-import { useRouter } from 'next/navigation'; // Certifique-se de usar apenas este hook
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
-    const router = useRouter(); // Inicializa o hook para navegação
+    const router = useRouter();
 
-    // Função para redirecionar para a página inicial
     const handleBackClick = () => {
-      router.push('/'); // Redireciona para a página de login (ou outra página padrão)
+        router.push('/');
     };
 
-    // Função para redirecionar para a tela principal
     const handleMainPageClick = () => {
-      router.push('/Home'); // Ajuste a rota para a página principal correta
+        router.push('/Home');
     };
 
     return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      {/* Onda azul no fundo */}
-      <svg className="absolute bottom-0 left-0 w-full h-full z-0" viewBox="0 0 1440 320" preserveAspectRatio="none">
-        <defs>
-          <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-            <stop offset="0%" style={{ stopColor: '#2168A9', stopOpacity: 1 }} />
-            <stop offset="52%" style={{ stopColor: '#6592E9', stopOpacity: 1 }} />
-            <stop offset="100%" style={{ stopColor: '#1E1E1C', stopOpacity: 1 }} />
-          </linearGradient>
-        </defs>
-        <path fill="url(#waveGradient)" fillOpacity="1" d="M0,160 C240,320 720,0 960,160 C1200,320 1680,0 1920,160 L1920,320 L0,320 Z"></path>
-      </svg> 
-           <Head>
-        <title>Inicio</title>
-        <meta name="description" content="Página inicial do app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+            {/* Onda azul no fundo */}
+            <svg className="absolute bottom-0 left-0 w-full h-full z-0" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <defs>
+                    <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                        <stop offset="0%" style={{ stopColor: '#2168A9', stopOpacity: 1 }} />
+                        <stop offset="52%" style={{ stopColor: '#6592E9', stopOpacity: 1 }} />
+                        <stop offset="100%" style={{ stopColor: '#1E1E1C', stopOpacity: 1 }} />
+                    </linearGradient>
+                </defs>
+                <path fill="url(#waveGradient)" fillOpacity="1" d="M0,160 C240,320 720,0 960,160 C1200,320 1680,0 1920,160 L1920,320 L0,320 Z"></path>
+            </svg>
 
-      <main className="w-full flex-1 flex flex-col items-center justify-center text-center">
-        <h1 className="text-5xl font-bold text-gray-800">
-          Bem-vindo ao Exclusive Finance
-        </h1>
+            <Head>
+                <title>Inicio</title>
+                <meta name="description" content="Página inicial do app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+           
+            <div className="bg-white bg-opacity-20 backdrop-blur-md text-white flex flex-col justify-center items-center p-10 rounded-lg w-[1100px] h-[500px] shadow-lg">
+      
+            <main className="w-full flex-1 flex flex-col items-center justify-center text-center z-10">
+                <h1 className="text-5xl font-bold text-gray-800">
+                    Bem-vindo ao Exclusive Finance
+                </h1>
 
-        <p className="mt-3 text-xl text-gray-600">
-          Cuidando de você e do seu bolso!
-        </p>
+                <p className="mt-3 text-xl text-gray-600">
+                    Cuidando de você e do seu bolso!
+                </p>
 
-        <div className="mt-5 flex flex-col items-center space-y-5">
-          <button
-            onClick={handleBackClick} // Botão para voltar para a página inicial (login ou home)
-            className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
-          >
-            Voltar para Página Inicial
-          </button>
+                <div className="mt-5 flex flex-col items-center space-y-5">
+                    <button
+                        onClick={handleBackClick}
+                        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
+                    >
+                        Voltar para Página Inicial
+                    </button>
 
-          <button
-            onClick={handleMainPageClick} // Botão para redirecionar para a tela principal
-            className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
-          >
-            Continuar
-          </button>
+                    <button
+                        onClick={handleMainPageClick}
+                        className="px-6 py-3 bg-gray-600 text-white rounded-lg shadow-lg hover:bg-gray-700 transition"
+                    >
+                        Continuar
+                    </button>
+                </div>
+            </main>
+          </div>
+            <footer className="w-full h-24 flex items-center justify-center border-t z-10">
+                <p className="text-gray-500">© 2024 EXF Exclusive Finance</p>
+            </footer>
         </div>
-      </main>
-
-      <footer className="w-full h-24 flex items-center justify-center border-t">
-        <p className="text-gray-500">© 2024 EXF Exclusive Finance</p>
-      </footer>
-    </div>
-  );
+    );
 }
